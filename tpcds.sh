867ecc352d948f2ff74c37d00e935200aba01291
Ryan Wei <rainmanwy@gmail.com>
2025-09-09 15:40:36 +0800

    Add truncate before loading data

--- tpcds.sh
+++ tpcds.sh
@@ -15,7 +15,7 @@ export TPC_DS_DIR
 log_time "TPC-DS test started"
 printf "\n"
 
-log_time "TPC-DS toolkit version is: V1.5"
+log_time "TPC-DS toolkit version is: V1.5_dev20250909"
 
 # Check that pertinent variables are set in the variable file.
 check_variables
