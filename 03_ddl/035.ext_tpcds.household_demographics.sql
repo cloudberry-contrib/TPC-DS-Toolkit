454fce1dfb8540d995a3e540fcb66a77c602f935
Ryan Wei <rainmanwy@gmail.com>
2025-10-29 18:27:02 +0800

    update ddl for external tables

--- 03_ddl/035.ext_tpcds.household_demographics.sql
+++ 03_ddl/035.ext_tpcds.household_demographics.sql
@@ -1,3 +1,3 @@
 CREATE EXTERNAL TABLE ext_tpcds.household_demographics (like :DB_SCHEMA_NAME.household_demographics)
 LOCATION (:LOCATION)
-FORMAT 'TEXT' (DELIMITER '|' NULL AS '' ESCAPE AS E'\\') ENCODING 'LATIN1';
+FORMAT 'CSV' (DELIMITER '|' NULL AS '' ESCAPE AS E'\\') ENCODING 'LATIN1';
