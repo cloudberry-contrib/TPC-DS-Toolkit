f7e1e00370e6a34dbbb2135a0c02a457ee5c6a4d
Ryan Wei <rainmanwy@gmail.com>
2025-11-18 16:53:53 +0800

    version

--- tpcds.sh
+++ tpcds.sh
@@ -15,7 +15,7 @@ export TPC_DS_DIR
 log_time "TPC-DS test started"
 printf "\n"
 
-log_time "TPC-DS toolkit version is: V2.0_dev20251116"
+log_time "TPC-DS toolkit version is: V2.1"
 
 # Check that pertinent variables are set in the variable file.
 check_variables
