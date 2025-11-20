77972fca49fa55fde460e9834a00c5f89d440fb2
Ryan Wei <rainmanwy@gmail.com>
2025-11-20 11:17:37 +0800

    debug

--- 07_single_user_reports/rollout.sh
+++ 07_single_user_reports/rollout.sh
@@ -19,9 +19,9 @@ log_time "Processing log files for reports."
 # Process SQL files in numeric order, using absolute paths
 for i in $(find "${PWD}" -maxdepth 1 -type f -name "*.${filter}.*.sql" -printf "%f\n" | sort -n); do
   if [ "${LOG_DEBUG}" == "true" ]; then
-    log_time "psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -t -q -A -f ${PWD}/${i} -v report_schema=${report_schema}"
+    log_time "psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -A -f ${PWD}/${i} -v report_schema=${report_schema}"
   fi
-  psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -t -q -A -f "${PWD}/${i}" -v report_schema=${report_schema}
+  psql ${PSQL_OPTIONS} -v ON_ERROR_STOP=1 -q -A -f "${PWD}/${i}" -v report_schema=${report_schema}
   echo ""
 done
 
